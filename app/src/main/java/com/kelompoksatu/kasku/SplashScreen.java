534fe0cc5eadaff311efc21d59acd5330c270d7c
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2021-10-07 11:47:48 +0700

    Initial commit

---
+++ app/src/main/java/com/kelompoksatu/kasku/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.kelompoksatu.kasku;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
